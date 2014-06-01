5e530d2cb1d13bd2ec59c0a1de74a82cf1bfd5ce
Blake Jakopovic <blake.jakopovic@gmail.com>
2014-06-01 23:01:02 +1000

    Updated Stlink Revision to v1.0.0

--- stlink.rb
+++ stlink.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Stlink < Formula
     homepage 'https://github.com/texane/stlink'
-    head 'https://github.com/texane/stlink.git', :revision => 'd650c9854f999d83f40d3e8c38b988b730de6368'
+    head 'https://github.com/texane/stlink.git', :revision => 'c84d8faf38e1d2069e84e0e86fe8ad3f054d36fd'
 
     depends_on 'pkg-config' => :build
     depends_on 'libusb'
